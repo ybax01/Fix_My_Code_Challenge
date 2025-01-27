2e149c3ad4734366093a9db8296c4515b1b62b70
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-01-27 14:27:30 +0100

    fixing

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" Square Class Module"""
 
 class square():
-    
+    """ Square Class """
+
     width = 0
     height = 0
-
-    
+ 
     def __init__(self, *args, **kwargs):
+        """ Instance creation"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,9 +17,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter calculation """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Printing attributes """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
