c9e7f867b1667ee7def1fd51b3d1bfb78152bb6d
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-01-27 14:33:12 +0100

    fixing

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -18,7 +18,7 @@ class Square():
         return self.width * self.height
 
     def perimeter_of_my_square(self):
-        """ Perimeter calculation """
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
